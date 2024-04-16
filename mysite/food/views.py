267867681e09a027bef2e66d0adede22553c317a
WiseVizo <mecoc1011@gmail.com>
2024-04-16 20:49:53 +0530

    food app and food model

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def greet(request):
+    return HttpResponse("Hello! Welcome to our food ordering platform.")
