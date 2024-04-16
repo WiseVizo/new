b4484ed2ba837ab483a4c3085ef01c710edc47b9
WiseVizo <mecoc1011@gmail.com>
2024-04-16 22:38:19 +0530

    details view implemented

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.list_all_foods, name="list_all_foods"),
+    path('<int:food_id>/', views.details, name="details"),
     path('greet/', views.greet, name="greet"),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from .models import Food
+from django.template import loader
 # Create your views here.
 def greet(request):
     return HttpResponse("Hello! Welcome to our food ordering platform.")
 
 def list_all_foods(request):
     food_list = Food.objects.all()
-    return HttpResponse(food_list)
+    template = loader.get_template("food/index.html")
+    context = {
+        "food_list" : food_list,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def details(request, food_id):
+    food = Food.objects.get(pk=food_id)
+    context = {
+        "food": food,
+    }
+    return render(request, "food/details.html", context)
