e1eb51572bf16cb5a63ca7548b25c4dd5e59d38c
WiseVizo <mecoc1011@gmail.com>
2024-04-16 21:28:40 +0530

    admin pannel and added models there

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.greet, name="greet"),
+    path('', views.list_all_foods, name="list_all_foods"),
+    path('greet/', views.greet, name="greet"),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Food
 # Create your views here.
 def greet(request):
     return HttpResponse("Hello! Welcome to our food ordering platform.")
+
+def list_all_foods(request):
+    food_list = Food.objects.all()
+    return HttpResponse(food_list)
