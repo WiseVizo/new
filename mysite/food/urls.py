ea514a88c69ca3b7719810b4d4b52551dcdca9df
WiseVizo <mecoc1011@gmail.com>
2024-04-17 17:56:53 +0530

    add items functionality added

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+app_name = 'food'
 urlpatterns = [
     path('', views.list_all_foods, name="list_all_foods"),
     path('<int:food_id>/', views.details, name="details"),
     path('greet/', views.greet, name="greet"),
+    # for adding items
+    path('add/', views.create_item, name="create_item"),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from .models import Food
 from django.template import loader
+from .forms import ItemForm
 # Create your views here.
 def greet(request):
     return HttpResponse("Hello! Welcome to our food ordering platform.")
@@ -20,3 +21,13 @@ def details(request, food_id):
         "food": food,
     }
     return render(request, "food/details.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("food:list_all_foods")
+    context = {
+        'form': form,
+    }
+    return render(request, "food/item_form.html", context)
