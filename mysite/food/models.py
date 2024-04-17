67640c4d54508562d968fdd747f2d021632ab6d9
WiseVizo <mecoc1011@gmail.com>
2024-04-17 10:59:44 +0530

    added some styles

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -5,6 +5,7 @@ class Food(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.CharField(max_length=600, default='https://www.foodista.com/sites/default/files/default_images/placeholder_rev.png')
 
     def __str__(self):
         return self.name
