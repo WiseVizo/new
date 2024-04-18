b1b00cfb28ceacd06920203cddb55b4a938812c2
WiseVizo <mecoc1011@gmail.com>
2024-04-19 00:29:04 +0530

    default pfps for custom users

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pictures')
+    image = models.ImageField(upload_to='profile_pictures', default="profile_pictures/placeholder_pfp.webp")
     location = models.CharField(max_length=100)
 
     def __str__(self) -> str:
