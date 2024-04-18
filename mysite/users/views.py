4b9a167dbad5122bdb79c3d1db03603ed69a01b1
WiseVizo <mecoc1011@gmail.com>
2024-04-18 18:47:56 +0530

    user auth implemented

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegistrationForm
 
 # Create your views here.
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Welcome {username} u have successfully registered!")
-            return redirect("food:list_all_foods")
+            return redirect("users:login")
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, "users/register.html", {'form': form,})
