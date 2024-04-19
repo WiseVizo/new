f38f838fa7935bff77d8ada69b564b2f6f677e7f
WiseVizo <mecoc1011@gmail.com>
2024-04-19 14:10:03 +0530

    few css touchups

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'food', 'static'),
+    os.path.join(BASE_DIR, 'users', 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
